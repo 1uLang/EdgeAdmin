54a3fa3e487af065e5e2511cfe856a24ad9ad5d7
dengpang <404821634@qq.com>
2021-06-11 10:29:06 +0800

    ddos +节点管理

--- internal/web/actions/default/assembly/idcOptions.go
+++ internal/web/actions/default/assembly/idcOptions.go
@@ -9,10 +9,17 @@ type IDCOptionsAction struct {
 }
 
 func (this *IDCOptionsAction) RunPost(params struct{}) {
-	this.Data["idcs"] = []map[string]interface{}{
-		{"id": 0, "name": "成都IDC"},
-		{"id": 1, "name": "杭州IDC"},
-		{"id": 2, "name": "济南IDC"},
+	//this.Data["idcs"] = []map[string]interface{}{
+	//	{"id": 0, "name": "成都IDC"},
+	//	{"id": 1, "name": "杭州IDC"},
+	//	{"id": 2, "name": "济南IDC"},
+	//}
+	data := make([]map[string]interface{}, len(idcMap))
+	for k, v := range idcMap {
+		data[k] = map[string]interface{}{
+			"id": k, "name": v,
+		}
 	}
+	this.Data["idcs"] = data
 	this.Success()
 }
--- internal/web/actions/default/assembly/options.go
+++ internal/web/actions/default/assembly/options.go
@@ -9,12 +9,19 @@ type OptionsAction struct {
 }
 
 func (this *OptionsAction) RunPost(params struct{}) {
-	this.Data["assemblys"] = []map[string]interface{}{
-		{"id": 0, "name": "DDoS防护"},
-		{"id": 1, "name": "云防火墙"},
-		{"id": 2, "name": "主机防护"},
-		{"id": 3, "name": "WEB漏洞扫描"},
-		{"id": 4, "name": "主机漏洞扫描"},
+	//this.Data["assemblys"] = []map[string]interface{}{
+	//	{"id": 0, "name": "DDoS防护"},
+	//	{"id": 1, "name": "云防火墙"},
+	//	{"id": 2, "name": "主机防护"},
+	//	{"id": 3, "name": "WEB漏洞扫描"},
+	//	{"id": 4, "name": "主机漏洞扫描"},
+	//}
+	data := make([]map[string]interface{}, len(typeMap))
+	for k, v := range typeMap {
+		data[k] = map[string]interface{}{
+			"id": k, "name": v,
+		}
 	}
+	this.Data["assemblys"] = data
 	this.Success()
 }
