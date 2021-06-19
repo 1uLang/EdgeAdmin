a19893acd59dc6b8728eb06a2c6c85771c28be2a
caoandong <y420475395@163.com>
2021-06-19 16:13:07 +0800

    回滚扫描任务 添加ddos 主机状态页面

--- web/views/@default/ddos/host/index.js
+++ web/views/@default/ddos/host/index.js
@@ -1,53 +1,75 @@
 Tea.context(function () {
-    // this.$delay(function () {
-    //     let that = this
-    //     sortTable(function () {
-    //         let groupIds = []
-    //         document.querySelectorAll("*[data-group-id]").forEach(function (element) {
-    //             groupIds.push(element.getAttribute("data-group-id"))
-    //         })
-    //         that.$post("/servers/components/groups/sort")
-    //             .params({
-    //                 groupIds: groupIds
-    //             })
-    //             .success(function () {
-    //                 teaweb.successToast("保存成功")
-    //             })
-    //     })
-    // })
+    this.severity = ''
+    this.dropNode=''
+    this.nShowState = 1 //当前显示的页面
 
-    this.createPolicy = function () {
-        teaweb.popup("/ddos/host/createPopup", {
+    this.tableState=1 //只有在nShowState==2时生效 屏蔽和连接列表的切换
+
+    this.onAddNodeIP = function () { 
+        teaweb.popup(Tea.url(".createPopup"), {
             callback: function () {
-                teaweb.success("保存成功", function () {
-                    teaweb.reload()
-                })
-            }
-        })
+              teaweb.success("保存成功", function () {
+                teaweb.reload();
+              });
+            },
+          });
+    }
+    //分页切换
+    this.changeState = function(state){
+      if(this.nShowState!=state){
+        this.nShowState = state
+      }
+    }
+    this.onOpenConfig = function (id) { 
+      this.changeState(2)
     }
 
-    this.updateGroup = function (groupId) {
-        teaweb.popup("/servers/components/groups/updatePopup?groupId=" + groupId, {
-            callback: function () {
-                teaweb.success("保存成功", function () {
-                    teaweb.reload()
-                })
-            }
-        })
+    //配置里面的列表切换
+    this.changeListState = function(state){
+      if(this.tableState!=state){
+        this.tableState = state
+      }
     }
 
-    this.deleteGroup = function (groupId) {
-        let that = this
-        teaweb.confirm("确定要删除这个分组吗？", function () {
-            that.$post("/servers/components/groups/delete")
-                .params({
-                    groupId: groupId
-                })
-                .success(function () {
-                    teaweb.success("删除成功", function () {
-                        teaweb.reload()
-                    })
-                })
-        })
+    //查询
+    this.onCheck = function () {
+      var inputValue = ""
+      if(this.tableState==1){
+        inputValue = document.getElementById("linkHostInput").value
+      }else{
+        inputValue = document.getElementById("unlinkHostInput").value
+      }
+      console.log(inputValue)
+      //todo
     }
+    //导出
+    this.onExport = function () {  
+      var inputValue = ""
+      if(this.tableState==1){
+        inputValue = document.getElementById("linkHostInput").value
+      }else{
+        inputValue = document.getElementById("unlinkHostInput").value
+      }
+      console.log(inputValue)
+      //todo
+    }
+    //全部释放 如果传入id 则单独释放 否则释放全部
+    this.onRelease = function (id) { 
+
+    }
+
+
+    this.tableData = [
+      {id:1,host:"118.112.240.0",user:"luobing",value1:"0.15",value2:"0.00",value3:"0.00",value4:"0.00",value5:"0.00",value6:"0.00",value7:"0.00",value8:"0.00",value9:"0.00",value10:"0.00",value11:"0.00",value12:"0.00",value13:"0.00"},
+      {id:1,host:"118.112.240.0",user:"luobing",value1:"0.15",value2:"0.00",value3:"0.00",value4:"0.00",value5:"0.00",value6:"0.00",value7:"0.00",value8:"0.00",value9:"0.00",value10:"0.00",value11:"0.00",value12:"0.00",value13:"0.00"},
+      {id:1,host:"118.112.240.0",user:"luobing",value1:"0.15",value2:"0.00",value3:"0.00",value4:"0.00",value5:"0.00",value6:"0.00",value7:"0.00",value8:"0.00",value9:"0.00",value10:"0.00",value11:"0.00",value12:"0.00",value13:"0.00"},
+      {id:1,host:"118.112.240.0",user:"luobing",value1:"0.15",value2:"0.00",value3:"0.00",value4:"0.00",value5:"0.00",value6:"0.00",value7:"0.00",value8:"0.00",value9:"0.00",value10:"0.00",value11:"0.00",value12:"0.00",value13:"0.00"},
+    ]
+
+    this.linkData=[
+      {id:1,host:"118.112.240.0",user:"118.112.240.0",status:"禁止689883471秒",res:"集群同步屏蔽 + 系统连接保护"},
+      {id:1,host:"118.112.240.0",user:"118.112.240.0",status:"禁止689883471秒",res:"集群同步屏蔽 + 系统连接保护"},
+      {id:1,host:"118.112.240.0",user:"118.112.240.0",status:"禁止689883471秒",res:"集群同步屏蔽 + 系统连接保护"},
+    ]
+
 })
