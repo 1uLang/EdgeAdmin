71b174330438b5a89bfc18078a9b721f219c2f83
1uLang <243971996@qq.com>
2021-08-11 18:14:11 +0800

    二级菜单功能实现

--- web/views/@default/users/features.js
+++ web/views/@default/users/features.js
@@ -1,26 +1,25 @@
 Tea.context(function () {
-	this.success = NotifyReloadSuccess("保存成功")
+    this.success = NotifyReloadSuccess("保存成功")
 
-	this.selectList = []
-    this.moveIndex=-1
+    this.moveIndex = ""
 
-	let that = this
+    let that = this
 
-    this.onRemoveTableItem=function (id,table) { 
-        if(id && table && table.length>0 ){
-            for(var index=0;index<table.length;index++){
-                if(table[index]==id){
-                    table.splice(index,1)
+    this.onRemoveTableItem = function (code, table) {
+        if (code && table && table.length > 0) {
+            for (var index = 0; index < table.length; index++) {
+                if (table[index] == code) {
+                    table.splice(index, 1)
                 }
             }
         }
         return table
     }
 
-    this.onGetTableItemInfo = function (id, table) {
-        if (table && id && table.length > 0 && id > 0) {
+    this.onGetTableItemInfo = function (code, table) {
+        if (table && code && table.length > 0) {
             for (var index = 0; index < table.length; index++) {
-                if (table[index].id == id) {
+                if (table[index].code == code) {
                     return table[index]
                 }
             }
@@ -28,10 +27,10 @@ Tea.context(function () {
         return null
     }
 
-	this.onCheckHadValue = function (id, table) {
-        if (table && id && table.length > 0 && id > 0) {
+    this.onCheckHadValue = function (code, table) {
+        if (table && code && table.length > 0) {
             for (var index = 0; index < table.length; index++) {
-                if (table[index] == id) {
+                if (table[index] == code) {
                     return true
                 }
             }
@@ -39,136 +38,112 @@ Tea.context(function () {
         return false
     }
 
-	this.getShowImageName=function (id) { 
-		
-		let bAllSelect = that.onCheckHadValue(id,that.selectList)
-		if(bAllSelect){
-			return "/images/select_select.png"
-		}else{
+    this.getShowImageName = function (code) {
+
+        let bAllSelect = that.onCheckHadValue(code, that.selectList)
+        if (bAllSelect) {
+            return "/images/select_select.png"
+        } else {
             let bSelect = false
-            let tempItem = that.onGetTableItemInfo(id,that.menuData)
-            if(tempItem){
-                for(var index=0;index<tempItem.children.length;index++){
-                    if(that.onCheckHadValue(tempItem.children[index].id,that.selectList)){
+            let tempItem = that.onGetTableItemInfo(code, that.features)
+            if (tempItem) {
+                for (var index = 0; index < tempItem.children.length; index++) {
+                    if (that.onCheckHadValue(tempItem.children[index].code, that.selectList)) {
                         bSelect = true
                         break
                     }
                 }
             }
-			if(bSelect){
-				return "/images/select_half_select.png"
-			}else{
-				return "/images/select_box.png"
-			}
-		}
-	}
+            if (bSelect) {
+                return "/images/select_half_select.png"
+            } else {
+                return "/images/select_box.png"
+            }
+        }
+    }
 
 
-	this.getItemShowImageName = function (id) { 
-		if(that.onCheckHadValue){
-			if(that.onCheckHadValue(id,that.selectList)){
-				return "/images/select_select.png"
-			}else{
-				return "/images/select_box.png"
-			}
-		}
-		return "/images/select_box.png" 
-	}
+    this.getItemShowImageName = function (id) {
+        if (that.onCheckHadValue) {
+            if (that.onCheckHadValue(id, that.selectList)) {
+                return "/images/select_select.png"
+            } else {
+                return "/images/select_box.png"
+            }
+        }
+        return "/images/select_box.png"
+    }
 
-    this.onShowChildItem=function (id) { 
-        var tempItem = this.onGetTableItemInfo(id,this.menuData)
-        if(tempItem){
+    this.onShowChildItem = function (code) {
+        var tempItem = this.onGetTableItemInfo(code, this.features)
+        if (tempItem) {
             tempItem.bShowChild = !tempItem.bShowChild
         }
     }
 
-    this.onMouseEnter = function (id) { 
-        this.moveIndex = id
+    this.onMouseEnter = function (code) {
+        this.moveIndex = code
     }
 
-    this.onMouseLeave = function (id) { 
-        this.moveIndex = -1
+    this.onMouseLeave = function () {
+        this.moveIndex = ""
     }
 
-    this.onSelectValue = function (id) { 
-        if(this.onCheckHadValue(id,this.selectList)){
-            this.onRemoveTableItem(id,this.selectList)
-            let tempItem = this.onGetTableItemInfo(id,this.menuData)
-            if(tempItem){
-                for(var index=0;index<tempItem.children.length;index++){
-                    this.onRemoveTableItem(tempItem.children[index].id,this.selectList)
+    this.onSelectValue = function (code) {
+        if (this.onCheckHadValue(code, this.selectList)) {
+            this.onRemoveTableItem(code, this.selectList)
+            let tempItem = this.onGetTableItemInfo(code, this.features)
+            if (tempItem) {
+                for (var index = 0; index < tempItem.children.length; index++) {
+                    this.onRemoveTableItem(tempItem.children[index].code, this.selectList)
                 }
             }
-        }else{
-            this.selectList.push(id)
-            let tempItem = this.onGetTableItemInfo(id,this.menuData)
-            if(tempItem){
-                for(var index=0;index<tempItem.children.length;index++){
-                    if(!this.onCheckHadValue(tempItem.children[index].id,this.selectList)){
-                        this.selectList.push(tempItem.children[index].id)
+        } else {
+            this.selectList.push(code)
+            let tempItem = this.onGetTableItemInfo(code, this.features)
+            if (tempItem) {
+                for (var index = 0; index < tempItem.children.length; index++) {
+                    if (!this.onCheckHadValue(tempItem.children[index].code, this.selectList)) {
+                        this.selectList.push(tempItem.children[index].code)
                     }
                 }
             }
         }
     }
 
-    this.onSelectChildValue = function (id,parentId) { 
-        if(this.onCheckHadValue(id,this.selectList)){
-            this.onRemoveTableItem(id,this.selectList)
-        }else{
-            this.selectList.push(id)
+    this.onSelectChildValue = function (code, parentId) {
+        if (this.onCheckHadValue(code, this.selectList)) {
+            this.onRemoveTableItem(code, this.selectList)
+        } else {
+            this.selectList.push(code)
         }
-        
+
         let bFindAll = true
-        let tempItem = this.onGetTableItemInfo(parentId,this.menuData)
-        if(tempItem){
-            for(var index=0;index<tempItem.children.length;index++){
-                if(!this.onCheckHadValue(tempItem.children[index].id,this.selectList)){
-                    console.log(tempItem.children[index].id)
+        let tempItem = this.onGetTableItemInfo(parentId, this.features)
+        if (tempItem) {
+            for (var index = 0; index < tempItem.children.length; index++) {
+                if (!this.onCheckHadValue(tempItem.children[index].code, this.selectList)) {
+                    console.log(tempItem.children[index].code)
                     bFindAll = false
                     break
                 }
             }
         }
-        if(bFindAll){
+        if (bFindAll) {
             this.selectList.push(parentId)
-        }else{
-            this.onRemoveTableItem(parentId,this.selectList)
+        } else {
+            this.onRemoveTableItem(parentId, this.selectList)
         }
     }
 
-	this.onSave=function () {
-		// req=>
-		// this.selectList
-	}
-
-
-	this.menuData = [
-		{
-			id:1,
-			name:"这是父节点1",
-			description:"这是副标题1",
-            bShowChild:false,
-			children:[
-				{id:2,name:"父节点1-id=>2",description:"这是副标题"},
-				{id:3,name:"父节点1-id=>3",description:"这是副标题"},
-				{id:4,name:"父节点1-id=>4",description:"这是副标题"},
-				{id:5,name:"父节点1-id=>5",description:"这是副标题"},
-				{id:6,name:"父节点1-id=>6",description:"这是副标题"},
-			],
-		},
-		{
-			id:7,
-			name:"这是父节点2",
-			description:"这是副标题2",
-            bShowChild:false,
-			children:[
-				{id:8, name:"父节点2-id=>8", description:"这是副标题"},
-				{id:9, name:"父节点2-id=>9", description:"这是副标题"},
-				{id:10,name:"父节点2-id=>10",description:"这是副标题"},
-				{id:11,name:"父节点2-id=>11",description:"这是副标题"},
-				{id:12,name:"父节点2-id=>12",description:"这是副标题"},
-			],
-		}
-	]
+    this.onSave = function () {
+        this.$post(".features").params({
+            UserId: this.userId,
+            Codes: this.selectList,
+        }).success(resp => {
+            if (resp.code === 200) {
+                teaweb.success("修改成功")
+            }
+        })
+    }
 })
