57cbbee1d7bb93c041930629716d2e5fc6a73783
hanchan <1256668725@qq.com>
2021-09-08 11:34:08 +0800

    增加nextcloud文件夹接口

--- internal/web/actions/default/databackup/dir.go
+++ internal/web/actions/default/databackup/dir.go
@@ -4,6 +4,7 @@ import (
 	"github.com/1uLang/zhiannet-api/nextcloud/model"
 	"github.com/1uLang/zhiannet-api/nextcloud/request"
 	"github.com/TeaOSLab/EdgeAdmin/internal/web/actions/actionutils"
+	"github.com/iwind/TeaGo/actions"
 )
 
 type DirAction struct {
@@ -17,7 +18,15 @@ func (this *DirAction) Init() {
 func (this *DirAction) RunPost(params struct {
 	Purl string
 	Name string
+
+	Must *actions.Must
 }) {
+	params.Must.
+		Field("id", params.Name).
+		Require("请输入文件名")
+	if params.Name == "" {
+		this.Fail("Purl不能为空")
+	}
 	// 获取token
 	token, err := model.QueryTokenByUID(this.AdminId(), 1)
 	if err != nil {
@@ -25,7 +34,7 @@ func (this *DirAction) RunPost(params struct {
 		return
 	}
 
-	err = request.CreateFoler(token,params.Purl,params.Name)
+	err = request.CreateFoler(token, params.Purl, params.Name)
 	if err != nil {
 		this.ErrorPage(err)
 		return
