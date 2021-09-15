c301db2d3c08f30562a43326f2de34e4dd348f71
dengpang <404821634@qq.com>
2021-09-15 09:43:40 +0800

    退出登录 操作记录

--- internal/web/actions/default/logout/index.go
+++ internal/web/actions/default/logout/index.go
@@ -1,7 +1,12 @@
 package logout
 
 import (
+	"github.com/TeaOSLab/EdgeAdmin/internal/oplogs"
+	"github.com/TeaOSLab/EdgeAdmin/internal/rpc"
+	"github.com/TeaOSLab/EdgeAdmin/internal/utils"
 	"github.com/TeaOSLab/EdgeAdmin/internal/web/helpers"
+	"github.com/TeaOSLab/EdgeCommon/pkg/rpc/dao"
+	"github.com/TeaOSLab/EdgeCommon/pkg/rpc/pb"
 	"github.com/iwind/TeaGo/actions"
 )
 
@@ -11,6 +16,21 @@ type IndexAction actions.Action
 func (this *IndexAction) Run(params struct {
 	Auth *helpers.UserShouldAuth
 }) {
+	rpcClient, err := rpc.SharedRPC()
+	if err != nil {
+		this.Fail("服务器出了点小问题：" + err.Error())
+	}
+	userResp, err := rpcClient.UserRPC().FindEnabledUser(rpcClient.Context(int64(params.Auth.AdminId())), &pb.FindEnabledUserRequest{UserId: int64(params.Auth.AdminId())})
+	if err != nil {
+		this.Fail("获取用户信息失败")
+		return
+	}
+	// 记录日志
+	err = dao.SharedLogDAO.CreateAdminLog(rpcClient.Context(int64(params.Auth.AdminId())), oplogs.LevelInfo, this.Request.URL.Path, "退出系统，用户名："+userResp.User.Username, this.RequestRemoteIP())
+	if err != nil {
+		utils.PrintError(err)
+	}
+
 	params.Auth.Logout()
 	this.RedirectURL("/")
 }
