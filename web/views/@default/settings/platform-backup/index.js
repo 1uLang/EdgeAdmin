be728af49facc5924a65eaa5df6ea912933822d2
caoandong <y420475395@163.com>
2021-09-22 16:42:32 +0800

    设置备份界面

--- web/views/@default/settings/platform-backup/index.js
+++ web/views/@default/settings/platform-backup/index.js
@@ -1,3 +1,29 @@
 Tea.context(function () {
 
+    this.onCopyFile = function(){
+
+    }
+
+    this.onCleanCopy = function(){
+        
+    }
+
+    this.onDownLoadFlie = function(item){
+
+    }
+
+    this.onResetFile = function(item){
+
+    }
+    this.onDeleteFile = function(item){
+
+    }
+
+    this.tableData = [
+        {id:1,fileName:"20211213.zip",copyTime:"2021-09-15 17:25:28",flieSize:"500k"},
+        {id:2,fileName:"20211213.zip",copyTime:"2021-09-15 17:25:28",flieSize:"500k"},
+        {id:3,fileName:"20211213.zip",copyTime:"2021-09-15 17:25:28",flieSize:"500k"},
+    ]
+
+
 })
