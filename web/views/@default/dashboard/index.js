ec20ebfc96ecd0a0dcac8a119494370a1e700c86
caoandong <y420475395@163.com>
2021-09-24 13:03:52 +0800

    业务概括界面修改

--- web/views/@default/dashboard/index.js
+++ web/views/@default/dashboard/index.js
@@ -1,74 +1,184 @@
 Tea.context(function () {
-	this.trafficTab = "hourly"
+	this.dayFrom = ""
+	this.dayTo = ""
+
+	this.titleName = []
 
 	this.$delay(function () {
-		this.reloadHourlyTrafficChart()
-	})
+        teaweb.datepicker("day-from-picker")
+        teaweb.datepicker("day-to-picker")
 
-	this.selectTrafficTab = function (tab) {
-		this.trafficTab = tab
-		if (tab == "hourly") {
-			this.$delay(function () {
-				this.reloadHourlyTrafficChart()
-			})
-		} else if (tab == "daily") {
-			this.$delay(function () {
-				this.reloadDailyTrafficChart()
-			})
+		for(var index = 0;index<this.tableData.length;index++){
+			this.titleName.push(this.tableData[index].name)
 		}
+
+		this.reloadPieTableChart()
+		this.reloadLineTableChart()
+    })
+
+	this.onTimeChange = function () {
+        let startTime = document.getElementById("day-from-picker").value
+        let endTime = document.getElementById("day-to-picker").value
+        if(this.dayFrom != startTime || this.dayTo != endTime) {
+            this.dayFrom = startTime
+            this.dayTo = endTime
+        }
+    }
+
+	this.onDownloadReport = function(){
+
 	}
 
-	this.reloadHourlyTrafficChart = function () {
-		let axis = teaweb.bytesAxis(this.hourlyTrafficStats, function (v) {
-			return v.bytes
-		})
-		let chartBox = document.getElementById("hourly-traffic-chart-box")
-		let chart = teaweb.initChart(chartBox)
-		let that = this
+	this.onOpenDetail = function(url){
+		window.location = url
+	}
+
+	this.reloadPieTableChart = function () {
+        let chartBox = document.getElementById("pie-chart-box")
+        let chart = echarts.init(chartBox)
+        let option = {
+            // title:{
+            //     text: '当前入侵威胁分布情况',
+            //     x:'left',
+            //     y: 'top',
+            //     textStyle: { 
+            //         fontSize: 16,
+            //         color: '#333',
+            //         fontWeight:"normal"
+            //     },
+            // },
+            //  图表距边框的距离,可选值：'百分比'¦ {number}（单位px）
+            // top: '16%',   // 等价于 y: '16%'
+            grid: {
+                top: 30,   // 等价于 y: '16%'
+                left: 15, 
+                right: 15,
+                bottom: 30,
+            },
+            legend: {
+                orient : 'horizontal',
+                x : 'center',
+                y : 'bottom',
+                data:this.titleName,
+            },
+			tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            color:['#2698fb','#26c4c3','#26c46f','#ffce51','#f95c74','#8f59dd','#474fc5','#3d4c7d'],
+			series: [
+                {
+                    name:"占比情况",
+                    type: 'pie',
+                    radius: "55%",
+                    center: ['50%', '50%'],
+                    data: this.tableData,
+                    itemStyle:{ 
+                        normal:{ 
+                            label:{ 
+                                show: true, 
+                                formatter: '{b} : {c} ({d}%)' 
+                            }, 
+                            labelLine :{show:true} 
+                        } 
+                    } 
+                }
+            ],
+			animation: false
+		}
+        chart.setOption(option)
+        chart.resize()
+    }
+
+	this.reloadLineTableChart = function () {
+		let chartBox = document.getElementById("line-chart-box")
+		let chart = echarts.init(chartBox)
 		let option = {
+			 title:{
+                text: '当日安全事件趋势情况',
+                x:'left',
+                y: 'top',
+                textStyle: { 
+                    fontSize: 16,
+                    color: '#333',
+                    fontWeight:"normal"
+                },
+            },
+            //  图表距边框的距离,可选值：'百分比'¦ {number}（单位px）
+            // top: '16%',   // 等价于 y: '16%'
+            grid: {
+                top: 30,   // 等价于 y: '16%'
+                left: 15, 
+                right: 15,
+                bottom: 30,
+                containLabel: true
+            },
 			xAxis: {
-				data: this.hourlyTrafficStats.map(function (v) {
-					return v.hour;
-				})
+                // name: 'Hour',
+                // boundaryGap值为false的时候，折线第一个点在y轴上
+                // boundaryGap: false,
+				data: this.lineTitle
 			},
 			yAxis: {
-				axisLabel: {
-					formatter: function (v) {
-						return v + axis.unit
-					}
-				}
-			},
+                // name: 'GB',
+                min:0, // 设置y轴刻度的最小值
+                // max:8,  // 设置y轴刻度的最大值
+                splitNumber:4,  // 设置y轴刻度间隔个数
+                // axisLine: {
+                //     lineStyle: {
+                //         // 设置y轴颜色
+                //         color: '#fff'
+                //     }
+                // },
+            },
 			tooltip: {
-				show: true,
-				trigger: "item",
-				formatter: function (args) {
-					let index = args.dataIndex
-					return that.hourlyTrafficStats[index].hour + "时：" + teaweb.formatBytes(that.hourlyTrafficStats[index].bytes)
-				}
-			},
-			grid: {
-				left: 40,
-				top: 10,
-				right: 20
+				trigger: "axis",
 			},
 			series: [
 				{
-					name: "流量",
-					type: "line",
-					data: this.hourlyTrafficStats.map(function (v) {
-						return v.bytes / axis.divider;
-					}),
-					itemStyle: {
-						color: "#9DD3E8"
+                    name: this.attCountData.ddosAtt.name,
+                    type: "line",
+                    data: this.attCountData.ddosAtt.value,
+                    itemStyle: {
+						color: "#2698fb"
 					},
-					lineStyle: {
-						color: "#9DD3E8"
+                    lineStyle: {
+                        color: "#2698fb"
+                    }
+                },
+				{
+                    name: this.attCountData.webAtt.name,
+                    type: "line",
+                    data: this.attCountData.webAtt.value,
+                    itemStyle: {
+						color: "#26c4c3"
 					},
-					areaStyle: {
-						color: "#9DD3E8"
-					},
-					smooth: true
-				}
+                    lineStyle: {
+                        color: "#26c4c3"
+                    }
+                },
+				{
+                    name: this.attCountData.netAtt.name,
+                    type: "line",
+                    data: this.attCountData.netAtt.value,
+                    itemStyle: {
+                        color: "#26c46f"
+                    },
+                    lineStyle: {
+                        color: "#26c46f"
+                    }
+                },
+				{
+                    name: this.attCountData.hostAtt.name,
+                    type: "line",
+                    data: this.attCountData.hostAtt.value,
+                    itemStyle: {
+                        color: "#ffce51"
+                    },
+                    lineStyle: {
+                        color: "#ffce51"
+                    }
+                },
 			],
 			animation: false
 		}
@@ -76,84 +186,33 @@ Tea.context(function () {
 		chart.resize()
 	}
 
-	this.reloadDailyTrafficChart = function () {
-		let axis = teaweb.bytesAxis(this.dailyTrafficStats, function (v) {
-			return v.bytes
-		})
-		let chartBox = document.getElementById("daily-traffic-chart-box")
-		let chart = teaweb.initChart(chartBox)
-
-		let that = this
-		let option = {
-			xAxis: {
-				data: this.dailyTrafficStats.map(function (v) {
-					return v.day;
-				})
-			},
-			yAxis: {
-				axisLabel: {
-					formatter: function (v) {
-						return v + axis.unit
-					}
-				}
-			},
-			tooltip: {
-				show: true,
-				trigger: "item",
-				formatter: function (args) {
-					let index = args.dataIndex
-					return that.dailyTrafficStats[index].day + "：" + teaweb.formatBytes(that.dailyTrafficStats[index].bytes)
-				}
-			},
-			grid: {
-				left: 40,
-				top: 10,
-				right: 20
-			},
-			series: [
-				{
-					name: "流量",
-					type: "line",
-					data: this.dailyTrafficStats.map(function (v) {
-						return v.bytes / axis.divider;
-					}),
-					itemStyle: {
-						color: "#9DD3E8"
-					},
-					lineStyle: {
-						color: "#9DD3E8"
-					},
-					areaStyle: {
-						color: "#9DD3E8"
-					},
-					smooth: true
-				}
-			],
-			animation: false
-		}
-		chart.setOption(option)
-		chart.resize()
-	}
-
-	/**
-	 * 升级提醒
-	 */
-	this.closeMessage = function (e) {
-		let target = e.target
-		while (true) {
-			target = target.parentNode
-			if (target.tagName.toUpperCase() == "DIV") {
-				target.style.cssText = "display: none"
-				break
-			}
+	
+	this.lineTitle = ["2点","4点","6点","8点","10点","12点","14点","16点"]
+	this.attCountData = {
+		ddosAtt:{
+			name:"DDos攻击",
+			value:[20,25,36,11,25,30,12,40],
+		},
+		webAtt:{
+			name:"WEB攻击",
+			value:[30,45,46,41,35,40,32,30],
+		},
+		netAtt:{
+			name:"网络入侵",
+			value:[10,15,26,31,45,10,22,20],
+		},
+		hostAtt:{
+			name:"主机入侵",
+			value:[40,35,16,21,15,20,42,10],
 		}
 	}
-
-	this.testData = {
-		nodeCount:20,
-		ddosCount:10,
-		scanCount:5,
-		nfwCount:12,
-		hidsCount:16,
-	}
+		
+	
+	
+	this.tableData = [
+		{id:1,name:"DDos攻击",value:1222,url:""},
+		{id:2,name:"WEB攻击",value:122222,url:""},
+		{id:3,name:"网络入侵",value:12225,url:""},
+		{id:4,name:"主机入侵",value:125555,url:""},
+	]
 })
