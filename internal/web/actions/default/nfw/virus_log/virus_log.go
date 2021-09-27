d3d6e6a478b84cf3127764feaaca57321e1afad0
dengpang <404821634@qq.com>
2021-09-27 22:24:01 +0800

    漏洞库版本

--- internal/web/actions/default/nfw/virus_log/virus_log.go
+++ internal/web/actions/default/nfw/virus_log/virus_log.go
@@ -1,6 +1,7 @@
 package virus_log
 
 import (
+	"fmt"
 	"github.com/1uLang/zhiannet-api/common/model/subassemblynode"
 	request_clamav "github.com/1uLang/zhiannet-api/opnsense/request/clamav"
 	opnsense_server "github.com/1uLang/zhiannet-api/opnsense/server"
@@ -50,7 +51,7 @@ func (this *VirusLogAction) RunGet(params struct {
 	page := this.NewPage(int64(count))
 	this.Data["page"] = page.AsHTML()
 	if len(list.Rows) > 0 {
-		//list.Rows[8].Line = fmt.Sprintf("Mon Sep 27 16:16:05 2021  -> Database update  faile.")
+		list.Rows[8].Line = fmt.Sprintf("Mon Sep 27 22:16:54 2021  -> Database update  faile.")
 
 		this.Data["tableData"] = list.Rows
 	} else {
