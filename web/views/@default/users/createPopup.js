b0190efb278cc70cb3b1604ddd8ea9b2fc688071
1uLang <243971996@qq.com>
2021-12-04 10:20:30 +0800

    创建平台用户/系统用户 页面bug

--- web/views/@default/users/createPopup.js
+++ web/views/@default/users/createPopup.js
@@ -16,13 +16,13 @@ Tea.context(function () {
         btnSubmit.disabled = "disabled";
     }
     this.submitFail = function (resp) {
-
         if (resp.message !== "") {
             teaweb.warn(resp.message)
             return
+        } else {
+            var error = resp.errors[0].messages[0]
+            teaweb.warn(error)
         }
-        var error = resp.errors[0].messages[0]
-        teaweb.warn(error)
         //获取表单提交按钮
         var btnSubmit = document.getElementById("submit-btn");
         //将表单提交按钮设置为不可用，可以避免用户再次点击提交按钮进行提交
